1247106b424953a5e0d324da6d11d00a50ec6c78
Emil Binoy <emilbinoy06@gmail.com>
2024-09-13 13:06:24 +0530

    updated

--- herba.js
+++ herba.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbar');
     const navbarLinks = document.querySelectorAll('#navbar a'); // Select all links in the navbar
     const parallaxSection = document.querySelector('.parallax');
-    const contentSection = document.querySelector('.content'); // Adjust this if your section has a different class or ID
+    const contentSection = document.querySelector('section'); // Adjust this if your section has a different class or ID
 
     const observerOptions = {
         root: null, // Use the viewport as the root
@@ -27,3 +27,32 @@ document.addEventListener('DOMContentLoaded', () => {
     // Observe the content section to trigger the color change
     observer.observe(contentSection);
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+} 
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
